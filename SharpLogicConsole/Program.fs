51ab24d5063103c1d72f2ab0a2ea0f49f5fe66b0
George Plotnikov <accembler@gmail.com>
2021-01-07 23:50:25 +0300

    add solution

---
+++ SharpLogicConsole/Program.fs
@@ -0,0 +1,10 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open SharpLogic
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    Ex
+    0 // return an integer exit code
