2fd35ad9ad79499fcdff067fc8f5062f549a24ac
George Plotnikov <accembler@gmail.com>
2021-01-08 19:27:36 +0300

    some updates for #3

--- SharpLogicConsole/Program.fs
+++ SharpLogicConsole/Program.fs
@@ -1,10 +1,12 @@
 ﻿// Learn more about F# at http://fsharp.org
 
 open System
-open SharpLogic
+open SharpLogic.Expressions
 
 [<EntryPoint>]
 let main argv =
     printfn "Hello World from F#!"
-    Ex
+    let expr = Expression.Add(Mul(Const 2, Var "x"), Var "y")
+    let expr1 = Expression.Add(Mul(Const 2, Const 0), Var "y")
+    let splifiedExpr = simplify expr1
     0 // return an integer exit code
