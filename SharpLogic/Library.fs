db94410f9165916e267f9f0b43013ec3b7fc325f
George Plotnikov <accembler@gmail.com>
2021-01-14 23:42:24 +0300

    fix simplify function

--- SharpLogic/Library.fs
+++ SharpLogic/Library.fs
@@ -1,27 +1,33 @@
 ﻿namespace SharpLogic
 
-module Expressions =
+module Formula =
 
-    type Expression =
+    type Formula =
         Var of string
         | Const of int
-        | Add of Expression * Expression
-        | Mul of Expression * Expression
+        | Add of Formula * Formula
+        | Mul of Formula * Formula
        
-    let simplify1 expression =
-        match expression with
+    let simplify1 formula =
+        match formula with
         Add(Const(m), Const(n)) -> Const(m+n)
-        | Mul(Const(m), Const(n)) -> Const(m+n)
-        | Add(Const(0), x) -> x
-        | Add(x, Const(0)) -> x
         | Mul(Const(0), x) -> Const(0)
         | Mul(x, Const(0)) -> Const(0)
+        | Mul(Const(m), Const(n)) -> Const(m+n)
         | Mul(Const(1), x) -> x
         | Mul(x, Const(1)) -> x
-        | _ -> expression;
+        | Add(Const(0), x) -> x
+        | Add(x, Const(0)) -> x
+        | _ -> formula;
 
-    let rec simplify expr =
-        match expr with
+    let rec simplify frm =
+        match frm with
             Add(e1, e2) -> simplify1(Add(simplify e1, simplify e2))
             | Mul(e1,e2) -> simplify1(Mul(simplify e1, simplify e2))
-            | _ -> simplify1 expr;
+            | _ -> simplify1 frm;
+
+    let isFormulaValid1 formula =
+        match formula with
+        Const(0) -> true
+        | Const(n) -> true
+        | _ -> false
