4680384bc564d6b45698d761c387b40394379944
George Plotnikov <accembler@gmail.com>
2021-01-15 22:22:06 +0300

    add formula validation #8

--- SharpLogic/Library.fs
+++ SharpLogic/Library.fs
@@ -7,13 +7,16 @@ module Formula =
         | Const of int
         | Add of Formula * Formula
         | Mul of Formula * Formula
+        | Neg of Formula
+        | Eq of Formula * Formula
+        | Impl of Formula * Formula
        
     let simplify1 formula =
         match formula with
         Add(Const(m), Const(n)) -> Const(m+n)
         | Mul(Const(0), x) -> Const(0)
         | Mul(x, Const(0)) -> Const(0)
-        | Mul(Const(m), Const(n)) -> Const(m+n)
+        | Mul(Const(m), Const(n)) -> Const(m*n)
         | Mul(Const(1), x) -> x
         | Mul(x, Const(1)) -> x
         | Add(Const(0), x) -> x
@@ -26,8 +29,15 @@ module Formula =
             | Mul(e1,e2) -> simplify1(Mul(simplify e1, simplify e2))
             | _ -> simplify1 frm;
 
-    let isFormulaValid1 formula =
+    let isFormulaValid formula =
         match formula with
-        Const(0) -> true
-        | Const(n) -> true
+        Var(n) -> true
+        | Neg(n) -> true
+        | Mul(Const(n), m) -> true
+        | Mul(n, Const(m)) -> true
+        | Mul(n, m) -> true
+        | Add(n, m) -> true
+        | Eq(n, m) -> true
+        | Impl(n, m) -> true
         | _ -> false
+            
--- SharpLogicConsole/Program.fs
+++ SharpLogicConsole/Program.fs
@@ -5,8 +5,9 @@ open SharpLogic.Formula
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    let expr = Formula.Add(Mul(Const 2, Var "x"), Var "y")
-    let expr1 = Formula.Add(Mul(Const 2, Const 0), Var "y")
-    let splifiedExpr = simplify expr1
+    let frm = Formula.Add(Mul(Const 2, Var "x"), Var "y")
+    let frm1 = Formula.Add(Mul(Const 2, Const 0), Var "y")
+    let frm2 = Formula.Const 1
+    let splifiedExpr = simplify frm1
+    let isFormulaValid = isFormulaValid frm2
     0 // return an integer exit code
