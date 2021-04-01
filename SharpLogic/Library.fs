5cabc8eddfac58e1cdbac77dbdbddd67a30355f9
George Plotnikov <accembler@gmail.com>
2021-04-01 23:07:51 +0300

    refactoring and fix one bug

--- SharpLogic/Library.fs
+++ SharpLogic/Library.fs
@@ -1,4 +1,6 @@
 ﻿namespace SharpLogic
+open SharpLogic.BasicAlgorithms
+open System.Collections.Generic
 
 module Formula =
 
@@ -58,6 +60,7 @@ module Formula =
         | Impl(n, m) -> [formula] @ BuildFormulaCalcList(n) @ BuildFormulaCalcList(m)
         | _ -> [ formula ]
 
+    //TODO: write unit tests
     let CalcFormula formula =
         match formula with
         | Conj(Const(X), Const(Y)) -> X && Y
@@ -71,3 +74,93 @@ module Formula =
             | (false, true) -> true
             | (false, false) -> true
         | _ -> false //TODO: what should be here?
+
+    //TODO: write unit tests
+    let BuildAllFormulasInterpritations formulaCalcList =
+        let vars = 
+            formulaCalcList
+                |> List.filter
+                (function 
+                    |Formula.Var(_) -> true
+                    | _ -> false
+                )
+        let data = List.init vars.Length (fun _ -> [ true; false ])
+        cartList data
+
+    //TODO: write unit tests
+    let VerboseTableuxCalculus (formulaCalcList: Formula list) (formulaInterpritations: bool list list) : string =
+        let sb = new System.Text.StringBuilder()
+        let subFormulasVals = new Dictionary<Formula, bool>()
+        let _saveFormulaValueAndPrint = fun formula value ->
+            if not(subFormulasVals.ContainsKey(formula)) then subFormulasVals.Add(formula, value) else subFormulasVals.Item(formula) <- value
+            sb.Append $"{value}\t" |> ignore
+            let len = ((VerboseFormula formula).Length / 8)
+            for _ = 1 to len do
+                sb.Append "\t" |> ignore
+        for i = 0 to formulaInterpritations.Length - 1 do
+            let rowVarValues = formulaInterpritations.Item i
+            let mutable j = 0
+            formulaCalcList |> List.iter
+                (fun f ->
+                            match f with
+                            | Formula.Var(_) ->
+                                sb.Append $"{rowVarValues.Item(j)} \t" |> ignore
+                                j <- j + 1
+                            | Formula.Neg(Var(x)) -> 
+                                let index = formulaCalcList |> List.findIndex(fun h -> h = Var(x))
+                                let calc = CalcFormula(Formula.Neg(Formula.Const(rowVarValues.Item(index))))
+                                _saveFormulaValueAndPrint f calc
+                            | Formula.Conj(Var(x), Var(y)) ->
+                                let indexX = formulaCalcList |> List.findIndex(fun h -> h = Var(x))
+                                let indexY = formulaCalcList |> List.findIndex(fun h -> h = Var(y))
+                                let calc = CalcFormula(Conj(Const(rowVarValues.Item(indexX)), Const(rowVarValues.Item(indexY))))
+                                _saveFormulaValueAndPrint f calc
+                            | Formula.Disj(Var(x), Var(y)) ->
+                                let indexX = formulaCalcList |> List.findIndex(fun h -> h = Var(x))
+                                let indexY = formulaCalcList |> List.findIndex(fun h -> h = Var(y))
+                                let calc = CalcFormula(Disj(Const(rowVarValues.Item(indexX)), Const(rowVarValues.Item(indexY))))
+                                _saveFormulaValueAndPrint f calc
+                            | Formula.Bic(Var(x), Var(y)) ->
+                                let indexX = formulaCalcList |> List.findIndex(fun h -> h = Var(x))
+                                let indexY = formulaCalcList |> List.findIndex(fun h -> h = Var(y))
+                                let calc = CalcFormula(Bic(Const(rowVarValues.Item(indexX)), Const(rowVarValues.Item(indexY))))
+                                _saveFormulaValueAndPrint f calc
+                            | Formula.Bic(Var(x), y) ->
+                                let rightVal = subFormulasVals.GetValueOrDefault(y)
+                                let indexX = formulaCalcList |> List.findIndex(fun h -> h = Var(x))
+                                let calc = CalcFormula(Bic(Const(rowVarValues.Item(indexX)), Const(rightVal)))
+                                _saveFormulaValueAndPrint f calc
+                            | Formula.Bic(x, Var(y)) ->
+                                let leftVal = subFormulasVals.GetValueOrDefault(x)
+                                let indexY = formulaCalcList |> List.findIndex(fun h -> h = Var(y))
+                                let calc = CalcFormula(Bic(Const(rowVarValues.Item(indexY)), Const(leftVal)))
+                                _saveFormulaValueAndPrint f calc
+                            | Formula.Bic(x, y) ->
+                                let leftVal = subFormulasVals.GetValueOrDefault(x)
+                                let rightVal = subFormulasVals.GetValueOrDefault(y)
+                                let calc = CalcFormula(Bic(Const(rightVal), Const(leftVal)))
+                                _saveFormulaValueAndPrint f calc
+                            | Formula.Impl(Var(x), Var(y)) ->
+                                let leftVal = formulaCalcList |> List.findIndex(fun h -> h = Var(x))
+                                let rightVal = formulaCalcList |> List.findIndex(fun h -> h = Var(y))
+                                let calc = CalcFormula(Impl(Const(rowVarValues.Item(leftVal)), Const(rowVarValues.Item(rightVal))))
+                                _saveFormulaValueAndPrint f calc
+                            | Formula.Impl(x, Var(y)) ->
+                                let leftVal = subFormulasVals.GetValueOrDefault(x)
+                                let rightVal = formulaCalcList |> List.findIndex(fun h -> h = Var(y))
+                                let calc = CalcFormula(Impl(Const(leftVal), Const(rowVarValues.Item(rightVal))))
+                                _saveFormulaValueAndPrint f calc
+                            | Formula.Impl(Var(x), y) ->
+                                let rightVal = subFormulasVals.GetValueOrDefault(y)
+                                let leftVal = formulaCalcList |> List.findIndex(fun h -> h = Var(x))
+                                let calc = CalcFormula(Impl(Const(rowVarValues.Item(leftVal)), Const(rightVal)))
+                                _saveFormulaValueAndPrint f calc
+                            | Formula.Impl(x, y) ->
+                                let leftVal = subFormulasVals.GetValueOrDefault(x)
+                                let rightVal = subFormulasVals.GetValueOrDefault(y)
+                                let calc = CalcFormula(Impl(Const(leftVal), Const(rightVal)))
+                                _saveFormulaValueAndPrint f calc
+                            | _ -> printf "unknown formula %A" f
+                )
+            sb.Append "\r\n" |> ignore
+        sb.ToString()
--- SharpLogicConsole/Program.fs
+++ SharpLogicConsole/Program.fs
@@ -1,7 +1,4 @@
-﻿open System
-open SharpLogic.Formula
-open SharpLogic.BasicAlgorithms
-open System.Collections.Generic
+﻿open SharpLogic.Formula
 
 [<EntryPoint>]
 let main argv =
@@ -13,95 +10,8 @@ let main argv =
     printf $"Formula is valid: [{isFormulaValid}]\n"
     printf "%s\r\n" "================" |> ignore 
     formulaCalcList |> List.map VerboseFormula |> List.iter(fun f -> printf "%s\t" f)
-
-    //TODO: extract as a separate function BuildAllFormulasInterpritations
-    let vars = 
-        formulaCalcList
-            |> List.filter
-            (function 
-                |Formula.Var(_) -> true
-                | _ -> false
-            )
-    let data = List.init vars.Length (fun _ -> [ true; false ])
-    let res = cartList data
-    //*****************
-
-    printf ""
-
-    //TODO: extract as a separate function VerboseTableuxCalculus
-    let subFormulasVals = new Dictionary<Formula, bool>()
-    let _saveFormulaValueAndPrint = fun formula value ->
-        if not(subFormulasVals.ContainsKey(formula)) then subFormulasVals.Add(formula, value) else subFormulasVals.Item(formula) <- value
-        printf "%b\t" value
-        let len = ((VerboseFormula formula).Length / 8)
-        for _ = 1 to len do
-           printf "\t"
-    for i = 0 to res.Length - 1 do
-        let rowVarValues = res.Item i
-        let mutable j = 0
-        formulaCalcList |> List.iter
-            (fun f ->
-                        match f with
-                        | Formula.Var(_) ->
-                            Console.Write(rowVarValues.Item(j).ToString() + "\t")
-                            j <- j + 1
-                        | Formula.Neg(Var(x)) -> 
-                            let index = formulaCalcList |> List.findIndex(fun h -> h = Var(x))
-                            let calc = CalcFormula(Formula.Neg(Formula.Const(rowVarValues.Item(index))))
-                            _saveFormulaValueAndPrint f calc
-                        | Formula.Conj(Var(x), Var(y)) ->
-                            let indexX = formulaCalcList |> List.findIndex(fun h -> h = Var(x))
-                            let indexY = formulaCalcList |> List.findIndex(fun h -> h = Var(y))
-                            let calc = CalcFormula(Conj(Const(rowVarValues.Item(indexX)), Const(rowVarValues.Item(indexY))))
-                            _saveFormulaValueAndPrint f calc
-                        | Formula.Disj(Var(x), Var(y)) ->
-                            let indexX = formulaCalcList |> List.findIndex(fun h -> h = Var(x))
-                            let indexY = formulaCalcList |> List.findIndex(fun h -> h = Var(y))
-                            let calc = CalcFormula(Disj(Const(rowVarValues.Item(indexX)), Const(rowVarValues.Item(indexY))))
-                            _saveFormulaValueAndPrint f calc
-                        | Formula.Bic(Var(x), Var(y)) ->
-                            let indexX = formulaCalcList |> List.findIndex(fun h -> h = Var(x))
-                            let indexY = formulaCalcList |> List.findIndex(fun h -> h = Var(y))
-                            let calc = CalcFormula(Bic(Const(rowVarValues.Item(indexX)), Const(rowVarValues.Item(indexY))))
-                            _saveFormulaValueAndPrint f calc
-                        | Formula.Bic(Var(x), y) ->
-                            let rightVal = subFormulasVals.GetValueOrDefault(y)
-                            let indexX = formulaCalcList |> List.findIndex(fun h -> h = Var(x))
-                            let calc = CalcFormula(Bic(Const(rowVarValues.Item(indexX)), Const(rightVal)))
-                            _saveFormulaValueAndPrint f calc
-                        | Formula.Bic(x, Var(y)) ->
-                            let leftVal = subFormulasVals.GetValueOrDefault(x)
-                            let indexY = formulaCalcList |> List.findIndex(fun h -> h = Var(y))
-                            let calc = CalcFormula(Bic(Const(rowVarValues.Item(indexY)), Const(leftVal)))
-                            _saveFormulaValueAndPrint f calc
-                        | Formula.Bic(x, y) ->
-                            let leftVal = subFormulasVals.GetValueOrDefault(x)
-                            let rightVal = subFormulasVals.GetValueOrDefault(y)
-                            let calc = CalcFormula(Bic(Const(rightVal), Const(leftVal)))
-                            _saveFormulaValueAndPrint f calc
-                        | Formula.Impl(Var(x), Var(y)) ->
-                            let indexX = formulaCalcList |> List.findIndex(fun h -> h = Var(x))
-                            let indexY = formulaCalcList |> List.findIndex(fun h -> h = Var(y))
-                            let calc = CalcFormula(Impl(Const(rowVarValues.Item(indexX)), Const(rowVarValues.Item(indexY))))
-                            _saveFormulaValueAndPrint f calc
-                        | Formula.Impl(x, Var(y)) ->
-                            let leftVal = subFormulasVals.GetValueOrDefault(x)
-                            let indexY = formulaCalcList |> List.findIndex(fun h -> h = Var(y))
-                            let calc = CalcFormula(Impl(Const(rowVarValues.Item(indexY)), Const(leftVal)))
-                            _saveFormulaValueAndPrint f calc
-                        | Formula.Impl(Var(x), y) ->
-                            let rightVal = subFormulasVals.GetValueOrDefault(y)
-                            let indexX = formulaCalcList |> List.findIndex(fun h -> h = Var(x))
-                            let calc = CalcFormula(Impl(Const(rowVarValues.Item(indexX)), Const(rightVal)))
-                            _saveFormulaValueAndPrint f calc
-                        | Formula.Impl(x, y) ->
-                            let leftVal = subFormulasVals.GetValueOrDefault(x)
-                            let rightVal = subFormulasVals.GetValueOrDefault(y)
-                            let calc = CalcFormula(Impl(Const(rightVal), Const(leftVal)))
-                            _saveFormulaValueAndPrint f calc
-                        | _ -> printf ""
-            )
-        printf "\r\n"
-    //**************
+    let formulaInterpritations = BuildAllFormulasInterpritations formulaCalcList
+    let output =  VerboseTableuxCalculus formulaCalcList formulaInterpritations
+    printf "%s" output
 
     0 // return an integer exit code
