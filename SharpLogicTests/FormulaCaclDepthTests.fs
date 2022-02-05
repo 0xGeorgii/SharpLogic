7263051db3cc5185548f5982ba9b96134dc2307c
George Plotnikov <accembler@gmail.com>
2022-02-05 23:05:54 +0300

    add use FsUnit User FsUnit instead of MSTest #18

--- SharpLogicTests/FormulaCaclDepthTests.fs
+++ SharpLogicTests/FormulaCaclDepthTests.fs
@@ -2,6 +2,7 @@
 
 open SharpLogic.Formula
 open Microsoft.VisualStudio.TestTools.UnitTesting
+open FsUnit
 
 //TODO: write additional cases
 [<TestClass>]
@@ -11,10 +12,10 @@ type FormulaCaclDepthTests() =
     member _.LengthIs1() =
         let frm = Formula.Var "N"
         let len = CalcFormulaDepth frm
-        Assert.AreEqual(1, len)
+        len |> should equal 1
 
     [<TestMethod>]
     member _.LengthIs3() =
         let frm = Formula.Disj(Conj(Var "N", Var "M"), Var "H")
         let len = CalcFormulaDepth frm
-        Assert.AreEqual(5, len)
+        len |> should equal 5
--- SharpLogicTests/IsFunctionValidTests.fs
+++ SharpLogicTests/IsFunctionValidTests.fs
@@ -2,6 +2,7 @@ namespace SharpLogicTests
 
 open SharpLogic.Formula
 open Microsoft.VisualStudio.TestTools.UnitTesting
+open FsUnit
 
 //TODO: write additional cases
 [<TestClass>]
@@ -11,64 +12,64 @@ type IsFunctionValidTests() =
     member _.VariableIsFormula() =
         let frm = Formula.Var "N"
         let isFormula = IsFormulaValid frm
-        Assert.IsTrue(isFormula)
+        isFormula |> should be True
 
     [<TestMethod>]
     member _.NegationIsFormula() =
         let frm = Formula.Neg(Formula.Var "N")
         let isFormula = IsFormulaValid frm
-        Assert.IsTrue(isFormula)
+        isFormula |> should be True
 
     [<TestMethod>]
     member _.ConjunctionIsFormula1() =
         let frm = Formula.Conj(Formula.Var "N", Formula.Var "M")
         let isFormula = IsFormulaValid frm
-        Assert.IsTrue(isFormula)
+        isFormula |> should be True
 
     [<TestMethod>]
     member _.ConjunctionIsFormula2() =
         let frm = Formula.Conj(Formula.Const true, Formula.Var "M")
         let isFormula = IsFormulaValid frm
-        Assert.IsTrue(isFormula)
+        isFormula |> should be True
 
     [<TestMethod>]
     member _.ConjunctionIsFormula3() =
         let frm = Formula.Conj(Formula.Var "N", Formula.Const false)
         let isFormula = IsFormulaValid frm
-        Assert.IsTrue(isFormula)
+        isFormula |> should be True
 
     [<TestMethod>]
     member _.DisjunctionIsFormula1() =
         let frm = Formula.Disj(Formula.Var "N", Formula.Var "M")
         let isFormula = IsFormulaValid frm
-        Assert.IsTrue(isFormula)
+        isFormula |> should be True
 
     [<TestMethod>]
     member _.DisjunctionIsFormula2() =
         let frm = Formula.Disj(Formula.Const true, Formula.Var "M")
         let isFormula = IsFormulaValid frm
-        Assert.IsTrue(isFormula)
+        isFormula |> should be True
 
     [<TestMethod>]
     member _.DisjunctionIsFormula3() =
         let frm = Formula.Disj(Formula.Var "N", Formula.Const true)
         let isFormula = IsFormulaValid frm
-        Assert.IsTrue(isFormula)
+        isFormula |> should be True
 
     [<TestMethod>]
     member _.BiconditionalIsFormula() =
         let frm = Formula.Bic(Formula.Var "N", Formula.Var "M")
         let isFormula = IsFormulaValid frm
-        Assert.IsTrue(isFormula)
+        isFormula |> should be True
 
     [<TestMethod>]
     member _.ImplicationIsFormula() =
         let frm = Formula.Impl(Formula.Var "N", Formula.Var "M")
         let isFormula = IsFormulaValid frm
-        Assert.IsTrue(isFormula)
+        isFormula |> should be True
 
     [<TestMethod>]
     member _.NegativeCases() =
         let frm = Formula.Const true
         let isFormula = IsFormulaValid frm
-        Assert.IsFalse(isFormula)
+        isFormula |> should be False
